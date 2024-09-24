0e93b8294c52ac4f0e9902f35d0c257f29f01f7b
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-09-25 02:56:08 +0300

    feat: misc changes
    Added tailwindcss support and current theme configuration.

--- .config/nvim/lua/plugins/languages.lua
+++ .config/nvim/lua/plugins/languages.lua
@@ -4,6 +4,7 @@ return {
   { import = "lazyvim.plugins.extras.lang.git" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.prisma" },
   { import = "lazyvim.plugins.extras.lang.sql" },
   { import = "lazyvim.plugins.extras.lang.tailwind" },
   { import = "lazyvim.plugins.extras.lang.terraform" },
