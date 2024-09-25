cd34439a47c06f030078db337409f76a74628908
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-09-25 03:13:32 +0300

    feat: nvim bulgarian phonetic layout keymap
    Added mapping of bulgarian phonetic letters to their latin counterparts
    in order for vim motions to work properly when switching layouts.
    Credit and many thanks to @mrgoblings for showing me this.
    
    Co-authored-by: MrGoblings <80454439+Mrgoblings@users.noreply.github.com>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 
+-- Bulgarian Phonetic layout
+vim.opt.langmap =
+  "явертъуиопшщасдфгхйклчзьцжбнмЯВЕРТЪУИОПШЩАСДФГХЙКЛЮЧЗѝЦЖБНМ;qwertyuiop[]asdfghjkl`zxcvbnmQWERTYUIOP{}ASDFGHJKL|~ZXCVBNM"
+
 -- Theme switcher
 vim.keymap.set("n", "<leader>th", ":Telescope themes<CR>", { noremap = true, silent = true, desc = "Theme Switcher" })
