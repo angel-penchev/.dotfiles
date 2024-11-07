2e2104dadd055ae09e80f077e1a2b918259868bc
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-11-07 14:47:49 +0200

    feat: wakatime plugin in neovim
    Added the WakaTime plugin to NeoVim.

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -7,4 +7,7 @@ return {
   { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
+
+  -- WakaTime
+  { "wakatime/vim-wakatime", lazy = false },
 }
