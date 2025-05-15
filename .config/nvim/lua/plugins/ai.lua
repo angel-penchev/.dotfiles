d0a0ce2bf152e7e130a3806677669fc83ba9d3d6
Angel Penchev <angel@penchev.dev>
2025-05-15 11:52:41 +0300

    feat: update nvim + configuration changes
    Updated Neovim plugins and related configuration files.

---
+++ .config/nvim/lua/plugins/ai.lua
@@ -0,0 +1,5 @@
+return {
+  -- GitHub Copilot
+  { import = "lazyvim.plugins.extras.ai.copilot" },
+  { import = "lazyvim.plugins.extras.ai.copilot-chat" },
+}
--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -1,8 +1,4 @@
 return {
-  -- GitHub Copilot
-  { import = "lazyvim.plugins.extras.coding.copilot" },
-  { import = "lazyvim.plugins.extras.coding.copilot-chat" },
-
   -- Code Snippets
   { import = "lazyvim.plugins.extras.coding.luasnip" },
   { import = "lazyvim.plugins.extras.coding.mini-comment" },
